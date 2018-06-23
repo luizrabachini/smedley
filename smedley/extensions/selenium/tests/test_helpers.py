e406de4a8c8d6ecaea1711bce610dcbc299027c2
Luiz Rabachini <luiz.rabachini@gmail.com>
2018-06-22 22:23:35 -0300

    Add tests to get element and validators

--- smedley/extensions/selenium/tests/test_helpers.py
+++ smedley/extensions/selenium/tests/test_helpers.py
@@ -1,4 +1,19 @@
+import mock
+import pytest
+
+from ..helpers import get_element
+
+
 class TestHelpers:
 
     def test_get_element(self):
-        assert False
+        find_element_by_test = mock.Mock()
+        element = mock.Mock(find_method='test')
+        browser = mock.Mock(find_element_by_test=find_element_by_test)
+        get_element(browser, element)
+        assert find_element_by_test.called
+
+    def test_get_element_not_found(self):
+        element = mock.Mock(find_method='test')
+        with pytest.raises(AttributeError):
+            get_element(object(), element)
--- smedley/extensions/selenium/tests/test_validators.py
+++ smedley/extensions/selenium/tests/test_validators.py
@@ -1,25 +1,78 @@
-class TestElementValidator:
+import mock
+import pytest
+
+from smedley.core.validators.exceptions import ValidationError
+from ..validators import (
+    ElementValidator,
+    SourceRegexValidator,
+    UrlRegexValidator
+)
+
+
+class BaseTestValidator:
+
+    def setup(self):
+        self.browser = mock.Mock()
+        self.context = {'browser': self.browser}
+
+
+class TestElementValidator(BaseTestValidator):
 
     def test__validate(self):
-        assert False
+        data = mock.Mock(find_method='test')
+        self.browser.get_element_by_test = mock.Mock()
+        assert not ElementValidator()._validate(
+            context=self.context,
+            data=data
+        )
 
-    def test__validate_error(self):
-        assert False
+    @mock.patch('smedley.extensions.selenium.validators.get_element')
+    def test__validate_error(self, get_element):
+        get_element.return_value = None
+        data = mock.Mock(find_method='test')
+        self.browser.get_element_by_test = mock.Mock()
+        with pytest.raises(ValidationError):
+            ElementValidator()._validate(
+                context=self.context,
+                data=data
+            )
 
 
-class TestSourceRegexValidator:
+class TestSourceRegexValidator(BaseTestValidator):
 
     def test__validate(self):
-        assert False
+        data = mock.Mock(pattern='.*<p>XPTO.*')
+        self.browser.page_source = '<html>Test <p>XPTO</p></html>'
+        assert not SourceRegexValidator()._validate(
+            context=self.context,
+            data=data
+        )
 
     def test__validate_error(self):
-        assert False
+        data = mock.Mock(pattern='.*<p>XPTO.*')
+        self.browser.page_source = '<html>Test <p>123XPTO</p></html>'
+        with pytest.raises(ValidationError):
+            SourceRegexValidator()._validate(
+                context=self.context,
+                data=data
+            )
 
 
-class TestUrlRegexValidator:
+class TestUrlRegexValidator(BaseTestValidator):
 
     def test__validate(self):
-        assert False
+        data = mock.Mock(pattern='http://.*/test.*')
+        self.browser.current_url = 'http://test.com/test?next=2'
+        assert not UrlRegexValidator()._validate(
+            context=self.context,
+            data=data
+        )
 
     def test__validate_error(self):
-        assert False
+        data = mock.Mock(pattern='http://.*/test.*')
+        self.browser.current_url = 'https://test.com/test?next=2'
+        with pytest.raises(ValidationError):
+            UrlRegexValidator()._validate(
+                context=self.context,
+                data=data
+            )
